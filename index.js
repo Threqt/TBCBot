7b899208fa6972fd1f6ea4c6e110d886cbf9365b
Threqt <45086473+Threqt@users.noreply.github.com>
2019-01-03 20:10:05 -0600

    a) Autorole

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ const bot = new Discord.Client({
   disableEverybody: true
 });
 
+bot.on(`guildMemberAdd`, member => {
+  console.log(`User ` + member.user.username + ` has joined`)
+  var role = member.guild.roles.find('name', 'Recruit')
+  member.addRole(role)
+});
+
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity("Over the beavers of Waterbeaver's colony", {
+  bot.user.setActivity("over the beavers of Waterbeaver's colony", {
     type: "WATCHING"
   });
 });
 
 bot.on("message", message => {
   if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
 
   if (message.content.indexOf(config.prefix) !== 0) return;
 
