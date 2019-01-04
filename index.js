5f8bffe4530902e5a7dd17ecfd02302baf3fe941
Threqt <45086473+Threqt@users.noreply.github.com>
2019-01-03 20:06:05 -0600

    Starting index.js..

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const botconfig = require("./botconfig")
-const Discord = require("discord.js")
+const botconfig = require("./botconfig");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({
+  disableEverybody: true
+});
 
 bot.on("ready", async () => {
-  console.log("ready to be ready")
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity("Over the beavers of Waterbeaver's colony", {
+    type: "WATCHING"
+  });
+});
+
+bot.on("message", message => {
+  if (message.author.bot) return;
+
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
 });
 
 bot.login(process.env.token);
