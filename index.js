b04cb73b93e450fbf43f646b697e277ed5851d0f
Threqt <45086473+Threqt@users.noreply.github.com>
2019-01-03 18:26:32 -0600

    Initial

---
+++ index.js
@@ -0,0 +1,8 @@
+const botconfig = require("./botconfig")
+const Discord = require("discord.js")
+
+bot.on("ready", async () => {
+  console.log("ready to be ready")
+});
+
+bot.login(process.env.token)
