94343a8ed374cb2abd3c9c1986ad4c578c3c6acf
Threqt <45086473+Threqt@users.noreply.github.com>
2019-04-21 07:48:18 -0500

    aha

--- index.js
+++ index.js
@@ -7,43 +7,40 @@ const bot = new Discord.Client({
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity("over JBM", {
+  bot.user.setActivity("over the Gangsters", {
     type: "WATCHING"
   });
 });
 
 bot.on("message", message => {
   if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
+  if  return;
 
   if (message.content.indexOf(config.prefix) !== 0) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
 
-  if (cmd === `ping`) {
-    return message.channel.send("pong");
-  } else
-  if (cmd === `botinfo`) {
+  if (cmd === `botinfo`) && (message.channel.type != "dm") {
     const embed = new Discord.RichEmbed()
-      .setAuthor(`JBM Bot`, bot.user.avatarURL)
+      .setAuthor(`Arc Bot`, bot.user.avatarURL)
       .setThumbnail(bot.user.avatarURL)
-      .addField(`Bot Name`, `JBM Bot`, true)
+      .addField(`Bot Name`, `Arc Bot`, true)
       .addField(`Created On`, bot.user.createdAt)
       .addField(`Version`, `1.0.0`, true)
-      .addField(`Developer`, `Threqt#3799`, true)
+      .addField(`Developer`, `Threqt#4377`, true)
       .setFooter("Prefix: ! | This bot is still in it's early phases", bot.user.avatarURL)
       .setTimestamp()
       .setColor(000000);
 
     return message.channel.send(embed)
   } else
-  if (cmd === `serverinfo`) {
+  if (cmd === `serverinfo`) && (message.channel.type != "dm") {
     let sicon = message.guild.iconURL
     let online = message.guild.members.filter(m => m.presence.status !== 'offline').size
     let categories = message.guild.channels.filter(m => m.type === 'category').size
     let embed2 = new Discord.RichEmbed()
-      .setAuthor("JBM Bot", bot.user.avatarURL)
+      .setAuthor("Arc Bot", bot.user.avatarURL)
       .setThumbnail(sicon)
       .addField("Owner", message.guild.owner, true)
       .addField("Created", message.guild.createdAt, true)
@@ -59,17 +56,29 @@ bot.on("message", message => {
 
     return message.channel.send(embed2)
   } else
-  if (cmd === `auction`){
-    let auctionrole = message.guild.roles.find(`name`, `[Auction Bidder]`)
-    if(message.member.roles.has(auctionrole.id)){
-      return message.member.removeRole(auctionrole.id).then(messag => {
-         message.reply("Successfully removed role Auction Bidder from you")
-      })
+  if (cmd === `answer` && message.channel.type === "dm") {
+    let answers = ['1', 'bunnybot', '2', 'pokemon', '3', 'game of thrones']
+    let rewards = ['1', 'gamer', '2', 'moon']
+    let ahh = message.content.slice(8, message.content.size)
+    let question = ahh.slice(0, 1)
+    console.log(question)
+    let answer = ahh.slice(2, ahh.size).toLowerCase()
+    console.log(answer)
+    let reward = ''
+    for (let i = 0; i < answers.length; i++){
+      if (question === answers[i]) {
+        console.log("1")
+        if (answer === answers[i + 1]) {
+          console.log("2")
+          reward = rewards[i + 1]
+          return message.reply(`Great. One of the ciphers is ${reward}`)
+        } else
+        return message.reply(`Wrong answer, try again.`)
+      }
     }
-    message.member.addRole(auctionrole.id).then(messag => {
-      return message.reply("Successfully added role Auction Bidder to you")
-    })
   }
 });
+
+
 
 bot.login(process.env.token);
