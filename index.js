7f08b40fb30b7a0e03fe814c75433c11793376c9
Threqt <45086473+Threqt@users.noreply.github.com>
2019-05-13 21:53:49 -0500

    update #2

--- index.js
+++ index.js
@@ -1,10 +1,79 @@
 const config = require("./botconfig");
 const Discord = require("discord.js");
-var version = "v0.1"
-var updateno = "1"
+var version = "v0.2"
+var updateno = "2"
 const bot = new Discord.Client({
   disableEverybody: true
 });
+
+//Async Functions
+async function poll(message, bot){
+  if(message.channel.name === "poll-creation"){
+    message.channel.send("Poll creation has started. Please type what you want the poll content to be.")
+    const filter = m => m.author.id === message.author.id;
+    let collected = await message.channel.awaitMessages(filter, {
+      max: 1,
+      time: 1200000,
+      errors: ['time']
+    })
+    if(!collected){
+      return message.channel.send("No content!")
+    }
+    if(collected.first().content.toLowerCase() == 'cancel'){
+      return message.channel.send("Cancelled.")
+    }
+    message.channel.send("Great! Now type the time in milliseconds on how long you want the poll to last. Use a hours - millisecond convertor if needed \n Common Times: \n 24 Hours: 86400000 milliseconds \n 1 hour: 3600000 milliseconds \n 30 minutes: 1800000 milliseconds ")
+    const filter2 = m => m.author.id === message.author.id;
+    let collected2 = await message.channel.awaitMessages(filter2, {
+      max: 1,
+      time: 1800000,
+      errors: ['time']
+    })
+    if(!collected2){
+      return message.channel.send("No content!")
+    }
+    if(collected2.first().content.toLowerCase() == 'cancel'){
+      return message.channel.send("Cancelled.")
+    }
+
+    let time1 = parseInt(collected2.first().content)
+
+    let pollEmbed = new Discord.RichEmbed()
+      .setAuthor("Tumbleweed Bot", bot.user.avatarURL)
+      .setThumbnail(bot.user.avatarURL)
+      .setTitle(`Poll by ${message.author.username}#${message.author.discriminator}`)
+      .setDescription(collected.first().content)
+      .setFooter(`Prefix: ! | Tumbleweed Bot | ${version}`, bot.user.avatarURL)
+      .setTimestamp();
+
+    await bot.channels.find(channel => channel.id === "577276201026977802").send(pollEmbed)
+    bot.channels.find(channel => channel.id === "577276201026977802").send("Vote 👍 if you agree, vote 👎 if you don't").then(async msg => {
+      await msg.react('👍')
+      await msg.react('👎')
+
+      const agree = '👍'
+      const disagree = '👎'
+
+      const filter = reaction => reaction.emoji.name === agree || reaction.emoji.name === disagree
+      const results = await msg.awaitReactions(filter, {time: time1})
+
+      let pollResults = new Discord.RichEmbed()
+        .setAuthor("Tumbleweed Bot", bot.user.avatarURL)
+        .setThumbnail(bot.user.avatarURL)
+        .setTitle(`Results of the poll by ${message.author.username}#${message.author.discriminator}`)
+        .setDescription(`Poll: ${collected.first().content}`)
+        .addField("👍", results.get(agree).count-1)
+        .addField("👎", results.get(disagree).count-1)
+        .setFooter(`Prefix: ! | Tumbleweed Bot | ${version}`, bot.user.avatarURL)
+        .setTimestamp();
+
+      await bot.channels.find(channel => channel.id === "577682587913420830").send(pollResults)
+    })
+  } else {
+    message.delete()
+    message.reply("Incorrect Channel!").then(r => r.delete(5000))
+  }
+}
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
@@ -72,11 +141,14 @@ bot.on("message", message => {
         .setThumbnail(bot.user.avatarURL)
         .setTitle(`Update Number ${updateno} | Version Number ${version}`)
         //•
-        .setDescription("Update Details: \n • Added autorole to the bot \n • Added update command to keep people informed on updates \n \n Planned features: \n • Poll command \n • Autorole using emojis \n • Chat moderation")
+        .setDescription("Update Details: \n • Added poll command to use for #public-polls \n \n Planned features: \n • Autorole using emojis \n • Chat moderation")
         .setFooter(`Prefix: ! | Tumbleweed Bot | ${version}`, bot.user.avatarURL)
         .setTimestamp();
       message.channel.send(update)
     }
+  } else
+  if(cmd === `createpoll`){
+    poll(message, bot)
   }
 });
 
